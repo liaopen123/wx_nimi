7e40c6ff846022c5899d1a885ef7bb8f488142d3
liaopen123 <liaopenghui@haier.com>
2020-05-28 18:11:55 +0800

    init wx mini

---
+++ pages/image/index.js
@@ -0,0 +1,87 @@
+// pages/image/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      msg:"廖鹏辉真帅~",
+      people:{
+        name:"廖鹏辉",
+        age:23,
+        sex:"male",
+        address:"handian",
+      },
+      checked:true,
+      list:[
+        {
+          id:1,
+          name:"廖鹏辉"
+        },
+        {
+          id:2,
+          name:"廖鹏辉2"
+        },
+        {
+          id:3,
+          name:"廖鹏辉3"
+        },
+      ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
